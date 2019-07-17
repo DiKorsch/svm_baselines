c8a6123889b5137b37fa3f1e6d4ffc2112dbc910
Dimitri Korsch <dimitri.korsch@uni-jena.de>
2019-07-17 15:03:48 +0200

    updated default info file location

--- l1_svm_parts/utils/arguments.py
+++ l1_svm_parts/utils/arguments.py
@@ -4,9 +4,11 @@ from chainer_addons.models import PrepareType
 
 from cvdatasets.utils import read_info_file
 
-DEFAULT_INFO_FILE="/home/korsch/Data/info.yml"
+import os
+DEFAULT_INFO_FILE=os.environ.get("DATA", "/home/korsch/Data/info.yml")
 
 info_file = read_info_file(DEFAULT_INFO_FILE)
+
 
 from l1_svm_parts.utils import ThresholdType, ClusterInitType
 
