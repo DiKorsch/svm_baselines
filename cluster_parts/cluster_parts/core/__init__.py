7c479d82be76426bbceebcdaf9410b9d436fd693
Dimitri Korsch <dimitri.korsch@uni-jena.de>
2019-11-04 11:42:34 +0100

    moved package to a separate git repo

--- cluster_parts/cluster_parts/core/__init__.py
+++
@@ -1,2 +0,0 @@
-from cluster_parts.core.extractor import BoundingBoxPartExtractor
-from cluster_parts.core.correction import Corrector
--- cluster_parts/cluster_parts/utils/__init__.py
+++
@@ -1,4 +0,0 @@
-from cluster_parts.utils._types.cluster import ClusterInitType
-from cluster_parts.utils._types.features import FeatureComposition
-from cluster_parts.utils._types.features import FeatureType
-from cluster_parts.utils._types.threshold import ThresholdType
